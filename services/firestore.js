d19943549e5af6eb2eece71cc82b3926945bcace
suminhand <tnaln47@gmail.com>
2025-09-06 13:50:15 +0900

    firebase connect test

--- services/firestore.js
+++ services/firestore.js
@@ -1,14 +1,6 @@
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
-var admin = require("firebase-admin");
-
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
 initializeApp({
     credential: applicationDefault()
   });
